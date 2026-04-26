ba90eb656dd7c5ecd86ab20ffa4dbcd94551b3a6
Sargun Vohra <sargunv@users.noreply.github.com>
2026-04-25 19:23:01 -0700

    Revert Hyprland close window shortcut from W back to Q

--- modules/home-hyprland/hyprland.nix
+++ modules/home-hyprland/hyprland.nix
@@ -120,7 +120,7 @@
       bindd = [
         "$mod, Return, Open terminal, exec, kitty"
         "$mod, Space, Open launcher, exec, vicinae toggle"
-        "$mod, W, Kill active window, killactive,"
+        "$mod, Q, Kill active window, killactive,"
         "$mod, P, Toggle pseudotile, pseudo,"
         "$mod, F, Toggle floating, togglefloating,"
         "$mod, Z, Toggle zen mode, fullscreen, 1"
