99efc67e276192eb3091631eea475271ffc4d7b7
Sargun Vohra <sargunv@users.noreply.github.com>
2026-05-13 23:26:40 -0700

    Configure starship git modules

--- modules/home-base/shell/starship.nix
+++ modules/home-base/shell/starship.nix
@@ -3,13 +3,21 @@
     enable = true;
     presets = [ "pure-preset" ];
     settings = {
-      format = "$username$hostname$directory$git_branch$git_state$git_status$cmd_duration$line_break$python$character";
+      format = "$username$hostname$directory$git_branch$git_state$git_metrics$git_status$cmd_duration$line_break$python$character";
 
       time = {
         disabled = false;
         style = "dimmed";
       };
       directory.truncate_to_repo = false;
+      git_status.ignore_submodules = true;
+      git_metrics = {
+        disabled = false;
+        ignore_submodules = true;
+        format = "([ +$added]($added_style))([ -$deleted]($deleted_style)) ";
+        added_style = "green";
+        deleted_style = "red";
+      };
       right_format = "$shell$time";
 
       shell = {
