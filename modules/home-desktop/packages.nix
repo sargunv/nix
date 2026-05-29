2f3c5e9e510e965cff8862f110c8c7035bbd6073
Sargun Vohra <sargunv@users.noreply.github.com>
2026-05-28 19:13:08 -0700

    spring cleaning

--- modules/home-desktop/packages.nix
+++ modules/home-desktop/packages.nix
@@ -3,28 +3,13 @@
 { pkgs, lib, ... }:
 
 let
-  inherit (pkgs.stdenv) isLinux isDarwin;
-
-  update-orion = pkgs.writeShellApplication {
-    name = "update-orion";
-    runtimeInputs = with pkgs; [ curl flatpak coreutils ];
-    text = ''
-      url="https://orionbrowser.com/download/latest.flatpak"
-      bundle=$(mktemp --suffix=.flatpak)
-      trap 'rm -f "$bundle"' EXIT
-      echo "downloading $url"
-      curl -fsSL --retry 3 -o "$bundle" "$url"
-      echo "installing..."
-      flatpak install --user --reinstall -y "$bundle"
-    '';
-  };
+  inherit (pkgs.stdenv) isLinux;
 in
 {
   home.packages =
     lib.optionals isLinux (with pkgs; [
       # Browsers
       vivaldi
-      update-orion # Orion is installed as a flatpak; this script reinstalls latest from Kagi
 
       # Communication
       beeper
